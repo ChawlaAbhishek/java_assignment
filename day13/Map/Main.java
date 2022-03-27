892c1bc96bd20e4f3e8abc786e184a5cf1cb421e
ChawlaAbhishek <97448896+ChawlaAbhishek@users.noreply.github.com>
2022-03-27 15:24:43 +0530

    Update Main.java

--- day13/Map/Main.java
+++ day13/Map/Main.java
@@ -1,22 +1,28 @@
 package day13.Map;
-
-
-
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+       // Employee e = new Employee();
+
         List<Student> ls = new ArrayList<>();
         ls.add(new Student(1,"Abhishek",700));
         ls.add(new Student(2,"Bobby",1000));
         ls.add(new Student(3,"Anshul",955));
         ls.add(new Student(4,"Hareram",799));
         ls.add(new Student(5,"Mohit Pandey",900));
+        
+        Stream<Student> ss =ls.stream();
+//                .map(student -> new Employee(student.getRoll(),student.getName(), student.getMarks())*1000)
+//                .toList()
 
-
-
-
+        List<Employee> e = ss.map(s -> new Employee(s.getRoll(),s.getName(),s.getMarks()*1000)).toList();
+        e.forEach(System.out::println);
     }
 }
+
